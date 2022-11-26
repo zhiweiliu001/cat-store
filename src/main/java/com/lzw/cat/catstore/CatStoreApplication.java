1d36da4737f763f189a69f95549e8071bc43fdc3
刘志伟 <1195728010@qq.com>
2022-11-26 20:45:09 +0800

    搭建项目

--- src/main/java/com/lzw/cat/catstore/CatStoreApplication.java
+++ src/main/java/com/lzw/cat/catstore/CatStoreApplication.java
@@ -9,6 +9,7 @@ public class CatStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatStoreApplication.class, args);
         System.out.println(11);
+
     }
 
 }
