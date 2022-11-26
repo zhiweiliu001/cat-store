212b41e55648ef87f83353ae3b60be50f60d8eab
刘志伟 <1195728010@qq.com>
2022-11-26 20:43:10 +0800

    搭建项目

---
+++ src/main/java/com/lzw/cat/catstore/CatStoreApplication.java
@@ -0,0 +1,14 @@
+package com.lzw.cat.catstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatStoreApplication.class, args);
+        System.out.println(11);
+    }
+
+}
---
+++ src/test/java/com/lzw/cat/catstore/CatStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzw.cat.catstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
